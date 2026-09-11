88e263c3a1f52d08913c0abc1f77d50b3ce338f8
KrushnrajZala <krushnrajzala07@gmail.com>
2026-09-11 20:52:29 +0530

    if

---
+++ if.py
@@ -0,0 +1,7 @@
+num = int(input("Enter Any Number: "))
+if(num%2==0):
+    print("The number is Even")
+elif(num%2!=0):
+    print("The number is Odd")
+else:
+    print("The number is neither Even nor Odd")
